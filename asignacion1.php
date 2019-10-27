d74569cde5467ef01cd3bdbfbcbd89a73a6d2aae
OuSeven07 <56278606+OuSeven07@users.noreply.github.com>
2019-10-27 17:46:17 -0300

    Inicio proyecto

---
+++ asignacion1.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<title>BRUH!</title>
+</head>
+<body>
+<?php
+    $x = 144;
+    $y = 999;
+
+    echo "<p>resultado de suma =</p>".  ($x + $y);
+
+    echo "<p>resultado de resta =</p>". ($x - $y);
+
+    echo "<p>resultado de multiplicacion =</p>". ($x * $y);
+
+    echo "<p>resultado de division =</p>". ($x / $y);
+
+    $nombre = Luilly Jesus Baez Rodriguez; 
+
+    echo "<p>What's up, Im $nombre</p>"
+?>
+</body>
+</html>
