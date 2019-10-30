0fcc406639095426c281c5886e4448112114faad
OuSeven07 <56278606+OuSeven07@users.noreply.github.com>
2019-10-30 13:58:53 -0300

    Update

--- asignacion1.php
+++ asignacion1.php
@@ -4,10 +4,21 @@
 <title>BRUH!</title>
 </head>
 <body>
+
+<div><h4>Welcome <?php echo $_SESSION['username']; ?></h4></div>
+
 <?php
+    
+    
+    $nombre = "Luilly Jesus Baez Rodriguez"; 
+
+    echo "What's up, Im $nombre";
+
     $x = 144;
     $y = 999;
 
+    echo "<p>Esta fue mi primera asignacion</p>";
+    
     echo "<p>resultado de suma =</p>".  ($x + $y);
 
     echo "<p>resultado de resta =</p>". ($x - $y);
@@ -15,10 +26,21 @@
     echo "<p>resultado de multiplicacion =</p>". ($x * $y);
 
     echo "<p>resultado de division =</p>". ($x / $y);
-
-    $nombre = Luilly Jesus Baez Rodriguez; 
-
-    echo "<p>What's up, Im $nombre</p>"
+    
+    
 ?>
+
+<ul>
+        
+        <li><a href="loginsystem.php">Login</a></li>
+        <li><a href="asignacion1.php">Asignacion</a></li>
+        <li><a href="Conversordemoneda.php">Conversor de moneda</a></li>
+        <li><a href="ipclienteservidor.php">Get your ip!</a></li>
+        <li><a href="URLactual.php">Ubicate!</a></li>
+        <li><a href="register.php">CREA TU CUENTA YA!</a></li>
+</ul>
+
+<footer>Bruh! &copy; 2019 </footer>
+
 </body>
 </html>
